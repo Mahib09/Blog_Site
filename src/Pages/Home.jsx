afed19f6b519eab1ed00f0badce09c9991c2e912
Mahib09 <Mahib.Ranamagar@triosstudent.com>
2024-05-03 17:49:43 -0400

    style size fixed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,9 +3,9 @@ export function Home() {
   return (
     <div className="container">
       <h1 className="text-8xl pt-16">mahib.log</h1>
-      <p className="text-xl text-gray-500 pt-5 mb-10">
-        Study Materials, Findings, Reusable Components, Blogs and everything in
-        between.
+      <p className="text-xl text-gray-500 mb-10">
+        Study Materials, Findings, Reusable Components,
+        <br /> Blogs and everything in between.
       </p>
       <TitleCard
         title="Why This Site?"
