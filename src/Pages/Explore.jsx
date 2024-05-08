1e764d2185559bb9bffa23063ffc0249890f06b3
Mahib09 <Mahib.Ranamagar@triosstudent.com>
2024-05-08 15:22:24 -0400

    Explore responsive made

--- src/Pages/Explore.jsx
+++ src/Pages/Explore.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 export function Explore() {
   return (
     <div className="container">
-      <h1 className="mt-16 dark:text-white">Explore</h1>
-      <p className="text-xl text-gray-500 dark:text-neutral-400">
+      <h1 className="text-4xl mt-12 sm:text-7xl dark:text-white">Explore</h1>
+      <p className="text-base text-gray-500 mt-2 mb-4 sm:text-xl dark:text-slate-200">
         Navigate through the pages.
       </p>
-      <p className="text-sm text-gray-500 dark:text-neutral-400">
+      <p className="text-xs sm:text-sm text-gray-500 dark:text-neutral-400">
         EXPLORE BY TAG
       </p>
       <div className="flex gap-2 mt-2">
--- src/components/TagButton.jsx
+++ src/components/TagButton.jsx
@@ -1,6 +1,6 @@
 export default function TagButton(props) {
   return (
-    <button className="p-3 bg-gray-300 rounded-lg text-md text-black hover:bg-gray-600 hover:text-gray-300 transition-all">
+    <button className="p-2 sm:p-3 bg-gray-300 rounded-lg text-sm sm:text-md text-black hover:bg-gray-600 hover:text-gray-300 transition-all">
       {props.name}
     </button>
   );
