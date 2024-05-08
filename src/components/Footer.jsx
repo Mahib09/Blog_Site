d04254792a3ba7879d9914be703188d8bb080416
Mahib09 <Mahib.Ranamagar@triosstudent.com>
2024-05-08 15:48:28 -0400

    lucide icon pack added and used

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
+import { Copyright, Github, Linkedin, Mail } from "lucide-react";
 export function Footer() {
   return (
     <div className="container">
       <div className="flex flex-col w-100vw items-center pt-10 gap-3">
-        <p className="text-base text-gray-500 sm:text-lg dark:text-neutral-400 dark:hover:text-white">
+        <p className="text-base text-gray-500 sm:text-lg dark:text-neutral-400">
+          <Copyright className="inline mr-2 mb-1 dark:text-neutral-400 cursor-text" />
           All rights reserved - mm.fyi
         </p>
         <div className="flex gap-2 text-gray-500 text-base pt-0 sm:text-lg dark:text-neutral-400 ">
@@ -11,6 +13,10 @@ export function Footer() {
             target="_blank"
             className="hover:text-black dark:hover:text-white"
           >
+            <Linkedin
+              size={18}
+              className="inline mr-1 mb-2 sm:h-[20px] sm:w-[20px]"
+            />
             LinkedIn
           </a>
           <a
@@ -18,6 +24,10 @@ export function Footer() {
             target="_blank"
             className="hover:text-black dark:hover:text-white"
           >
+            <Github
+              size={18}
+              className="inline mr-1 mb-2 sm:h-[20px] sm:w-[20px]"
+            />
             Github
           </a>
           <a
@@ -25,6 +35,10 @@ export function Footer() {
             target="_blank"
             className="hover:text-black dark:hover:text-white"
           >
+            <Mail
+              size={18}
+              className="inline mr-1 mb-2 sm:h-[20px] sm:w-[20px]"
+            />
             Email
           </a>
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { Home, Telescope } from "lucide-react";
 import { Link } from "react-router-dom";
 let logo = "<mm/>";
 export function Header() {
@@ -14,12 +15,20 @@ export function Header() {
           to="/"
           className="text-base ml-auto text-gray-500 hover:text-black sm:text-lg dark:text-neutral-400 dark:hover:text-white"
         >
+          <Home
+            size={18}
+            className="inline mr-1 mb-2 sm:h-[20px] sm:w-[20px]"
+          />
           Home
         </Link>
         <Link
           to="/Explore"
           className="text-base text-gray-500 hover:text-black sm:text-lg dark:text-neutral-400 dark:hover:text-white"
         >
+          <Telescope
+            size={18}
+            className="inline mr-1 mb-2 sm:h-[20px] sm:w-[20px]"
+          />
           Explore
         </Link>
       </div>
