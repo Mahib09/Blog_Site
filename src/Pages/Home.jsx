441a5a9976fb01778c0a9f46a9ebd761be72ea9b
Mahib09 <Mahib.Ranamagar@triosstudent.com>
2024-05-08 16:33:07 -0400

    basic homepage animation added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,37 @@
+import { useRef } from "react";
 import { TitleCard } from "../components/TitleCard";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
 export function Home() {
+  const animate1 = useRef(null);
+  const animate2 = useRef(null);
+  useGSAP(() => {
+    gsap.from(animate1.current, {
+      duration: 1,
+      y: 30,
+      opacity: 0,
+    });
+    gsap.from(animate2.current, {
+      duration: 1,
+      x: -50,
+      opacity: 0,
+    });
+  }, []);
+
   return (
     <div className="container">
-      <h1 className="text-4xl mt-10 sm:text-7xl dark:text-white ">mahib.log</h1>
-      <p className="text-base text-gray-500 mt-2 mb-4 sm:text-lg dark:text-neutral-400">
+      <h1
+        ref={animate2}
+        className="text-4xl mt-10 sm:text-7xl dark:text-white relative "
+      >
+        mahib.log
+      </h1>
+      <p
+        ref={animate1}
+        className="text-base text-gray-500 mt-2 mb-4 sm:text-lg dark:text-neutral-400"
+      >
         Study Materials, Findings, Reusable Components, Blogs and everything in
         between.
       </p>
