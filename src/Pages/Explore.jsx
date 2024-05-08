2cd673b577dc156396d76e41431d10e2c702dda1
Mahib09 <Mahib.Ranamagar@triosstudent.com>
2024-05-07 23:55:36 -0400

    Explore page dark mode made

--- src/Pages/Explore.jsx
+++ src/Pages/Explore.jsx
@@ -3,9 +3,13 @@ import { Link } from "react-router-dom";
 export function Explore() {
   return (
     <div className="container">
-      <h1 className="mt-16">Explore</h1>
-      <p className="text-xl text-gray-500">Navigate through the pages.</p>
-      <p className="text-sm text-gray-500">EXPLORE BY TAG</p>
+      <h1 className="mt-16 dark:text-white">Explore</h1>
+      <p className="text-xl text-gray-500 dark:text-neutral-400">
+        Navigate through the pages.
+      </p>
+      <p className="text-sm text-gray-500 dark:text-neutral-400">
+        EXPLORE BY TAG
+      </p>
       <div className="flex gap-2 mt-2">
         <Link to="/">
           <TagButton name="All" />
