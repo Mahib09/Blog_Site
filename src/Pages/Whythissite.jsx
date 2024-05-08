416aaccf02daa8f3cf3a55b818670acae875b140
Mahib09 <Mahib.Ranamagar@triosstudent.com>
2024-05-08 15:17:42 -0400

    Blog responsive and dark mode made

--- src/Pages/Whythissite.jsx
+++ src/Pages/Whythissite.jsx
@@ -2,9 +2,11 @@ export function Whythissite() {
   return (
     <div className="container">
       <div className="mt-16">
-        <h1>Why This Site?</h1>
-        <p className="text-xl text-gray-500">May 1, 2024</p>
-        <p>
+        <h1 className="text-4xl sm:text-7xl dark:text-white">Why This Site?</h1>
+        <p className="text-base text-gray-500 mt-2 mb-4 sm:text-lg dark:text-neutral-400">
+          May 1, 2024
+        </p>
+        <p className="text-base text-slate-900 mt-2 mb-4 sm:text-xl dark:text-slate-200">
           As I was freshening up on React, I was looking at doing some projects
           that would help me refresh on React, which I wanted to do as I am
           going to the 4th and final semester, which has advanced web
@@ -13,8 +15,10 @@ export function Whythissite() {
           save some reusable components or material that I make for future
           reference. So, this site is going to be all that.
         </p>
-        <h2>What will this site include?</h2>
-        <p>
+        <h2 className="text-2xl sm:text-5xl dark:text-white">
+          What will this site include?
+        </h2>
+        <p className="text-base text-slate-900 mt-2 mb-4 sm:text-xl dark:text-slate-200">
           Blogs <br />
           Study Refrences and Findings <br />
           Reusable Components <br />
