7fcc041796fb3847afd228d0b85f080367812212
Mahib09 <magarmahib@gmail.com>
2024-06-05 21:45:17 -0400

    algorithms scroll bar items madee

--- src/Pages/AllBlogs/Cheatsheets/Algorithms.jsx
+++ src/Pages/AllBlogs/Cheatsheets/Algorithms.jsx
@@ -1,7 +1,24 @@
+import TagButton from "../../../components/TagButton";
 export default function Algorithms() {
   return (
     <div className="container">
       <h1>Algorithms Cheatsheet</h1>
+      <p className="subtitle">May 31, 2024</p>
+      <p>Algorithm cheatsheet for future references.</p>
+      <div className="slider flex flex-row gap-2 overflow-x-auto ">
+        <TagButton name="Math Algorithms" />
+        <TagButton name="Bit Manupulation" />
+        <TagButton name="Hashes" />
+        <TagButton name="Math Algorithms" />
+        <TagButton name="Bit Manupulation" />
+        <TagButton name="Hashes" />
+        <TagButton name="Math Algorithms" />
+        <TagButton name="Bit Manupulation" />
+        <TagButton name="Hashes" />
+        <TagButton name="Math Algorithms" />
+        <TagButton name="Bit Manupulation" />
+        <TagButton name="Hashes" />
+      </div>
     </div>
   );
 }
