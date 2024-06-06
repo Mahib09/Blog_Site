c8d56bdb6dc409713f7af04535aaf9a669dbabaa
Mahib09 <magarmahib@gmail.com>
2024-06-05 21:49:40 -0400

    scroll bar hidden

--- src/Pages/AllBlogs/Cheatsheets/Algorithms.jsx
+++ src/Pages/AllBlogs/Cheatsheets/Algorithms.jsx
@@ -5,7 +5,7 @@ export default function Algorithms() {
       <h1>Algorithms Cheatsheet</h1>
       <p className="subtitle">May 31, 2024</p>
       <p>Algorithm cheatsheet for future references.</p>
-      <div className="slider flex flex-row gap-2 overflow-x-auto ">
+      <div className="hide-scrollbar flex flex-row gap-2 overflow-x-auto ">
         <TagButton name="Math Algorithms" />
         <TagButton name="Bit Manupulation" />
         <TagButton name="Hashes" />
